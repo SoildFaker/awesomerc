33e5c410d2bc941fe615dc57a6de3ffa1b15b3d7
SoildFaker <enbinli@outlook.com>
2016-02-22 05:46:41 +0000

    brightness adjust

---
+++ light.sh
@@ -0,0 +1,25 @@
+arg=$1
+dx=$2
+if [ $arg = "-aq" ]; then
+    if [ ! -w /sys/class/backlight/intel_backlight/brightness ];then 
+        echo "enbin@123" | sudo -S chmod 777 /sys/class/backlight/intel_backlight/brightness 
+    fi 
+    read bright < '/sys/class/backlight/intel_backlight/brightness' 
+     
+    v=$(($bright + $dx )) 
+
+    if [ $v -lt 900 ]; then
+        echo $v > /sys/class/backlight/intel_backlight/brightness 
+    fi 
+elif [ $arg = "-sq" ]; then
+    if [ ! -w /sys/class/backlight/intel_backlight/brightness ];then 
+        echo "enbin@123" | sudo -S chmod 777 /sys/class/backlight/intel_backlight/brightness 
+    fi 
+    read bright < '/sys/class/backlight/intel_backlight/brightness' 
+     
+    v=$(($bright - $dx)) 
+
+    if [ $v -gt 0 ]; then
+        echo $v > /sys/class/backlight/intel_backlight/brightness 
+    fi
+fi
