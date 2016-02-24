b2413d935c381e89cb87f48d2f738f2923a66ea3
SoildFaker <enbinli@outlook.com>
2016-02-24 23:54:49 +0000

    update light.sh

--- light.sh
+++ light.sh
@@ -1,5 +1,6 @@
 arg=$1
 dx=$2
+
 if [ $arg = "-aq" ]; then
     if [ ! -w /sys/class/backlight/intel_backlight/brightness ];then 
         echo "yourpassword" | sudo -S chmod 777 /sys/class/backlight/intel_backlight/brightness 
