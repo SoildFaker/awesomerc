2a9592959e6350cba856a9f0e09909871f1dc2d5
soildfaker <enbinli@outlook.com>
2016-12-24 13:31:19 +0800

    modified file

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -15,8 +15,8 @@ white = "#ffffff"
 
 theme = {}
 
-theme.wallpaper = "~/.config/awesome/themes/default/wallpapers/wallpaper-2552963.jpg"
 themes_dir = "~/.config/awesome/themes/"
+theme.wallpaper = themes_dir .. "/default/wallpapers/background.jpg"
 theme.icon_theme = nil
 theme.tasklist_disable_icon = true
 theme.font                                  = "sans 8"
@@ -92,85 +92,6 @@ theme.menu_bg_normal    = "#222222"
 -- theme.menu_border_width = ""
 theme.menu_height       = "16"
 theme.menu_width        = "140"
-
---{{--- Theme icons ------------------------------------------------------------------------------------------
-
-theme.awesome_icon                              = themes_dir .. "/default/icons/powerarrow/awesome-icon.png"
-theme.clear_icon                                = themes_dir .. "/default/icons/powerarrow/clear.png"
--- theme.clear_icon                                = themes_dir .. "/default/icons/powerarrow/llauncher.png"
-theme.menu_submenu_icon                         = themes_dir .. "/default/icons/powerarrow/submenu.png"
-theme.tasklist_floating_icon                    = themes_dir .. "/default/icons/powerarrow/floatingm.png"
-theme.titlebar_close_button_focus               = themes_dir .. "/default/icons/powerarrow/close_focus.png"
-theme.titlebar_close_button_normal              = themes_dir .. "/default/icons/powerarrow/close_normal.png"
-theme.titlebar_ontop_button_focus_active        = themes_dir .. "/default/icons/powerarrow/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active       = themes_dir .. "/default/icons/powerarrow/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive      = themes_dir .. "/default/icons/powerarrow/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive     = themes_dir .. "/default/icons/powerarrow/ontop_normal_inactive.png"
-theme.titlebar_sticky_button_focus_active       = themes_dir .. "/default/icons/powerarrow/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active      = themes_dir .. "/default/icons/powerarrow/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive     = themes_dir .. "/default/icons/powerarrow/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive    = themes_dir .. "/default/icons/powerarrow/sticky_normal_inactive.png"
-theme.titlebar_floating_button_focus_active     = themes_dir .. "/default/icons/powerarrow/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active    = themes_dir .. "/default/icons/powerarrow/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive   = themes_dir .. "/default/icons/powerarrow/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive  = themes_dir .. "/default/icons/powerarrow/floating_normal_inactive.png"
-theme.titlebar_maximized_button_focus_active    = themes_dir .. "/default/icons/powerarrow/maximized_focus_active.png"
-theme.titlebar_maximized_button_normal_active   = themes_dir .. "/default/icons/powerarrow/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_dir .. "/default/icons/powerarrow/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_inactive = themes_dir .. "/default/icons/powerarrow/maximized_normal_inactive.png"
-theme.taglist_squares_sel                       = themes_dir .. "/default/icons/powerarrow/square_sel.png"
-theme.taglist_squares_unsel                     = themes_dir .. "/default/icons/powerarrow/square_unsel.png"
-theme.layout_floating                           = themes_dir .. "/default/icons/powerarrow/floating.png"
-theme.layout_tile                               = themes_dir .. "/default/icons/powerarrow/tile.png"
-theme.layout_tileleft                           = themes_dir .. "/default/icons/powerarrow/tileleft.png"
-theme.layout_tilebottom                         = themes_dir .. "/default/icons/powerarrow/tilebottom.png"
-theme.layout_tiletop                            = themes_dir .. "/default/icons/powerarrow/tiletop.png"
-theme.widget_temp                              = themes_dir .. "/default/icons/temp.png"
-theme.widget_uptime                             = themes_dir .. "/default/icons/ac.png"
-theme.widget_cpu                                = themes_dir .. "/default/icons/cpu.png"
-theme.widget_weather                            = themes_dir .. "/default/icons/dish.png"
-theme.widget_fs                                 = themes_dir .. "/default/icons/fs.png"
-theme.widget_mem                                = themes_dir .. "/default/icons/mem.png"
-theme.widget_fs                                 = themes_dir .. "/default/icons/fs.png"
-theme.widget_note                               = themes_dir .. "/default/icons/note.png"
-theme.widget_note_on                            = themes_dir .. "/default/icons/note_on.png"
-theme.widget_netdown                            = themes_dir .. "/default/icons/net_down.png"
-theme.widget_netup                              = themes_dir .. "/default/icons/net_up.png"
-theme.widget_mail                               = themes_dir .. "/default/icons/mail.png"
-theme.widget_batt                               = themes_dir .. "/default/icons/bat.png"
-theme.widget_clock                              = themes_dir .. "/default/icons/clock.png"
-theme.widget_vol                                = themes_dir .. "/default/icons/spkr.png"
-
-theme.widget_mail                               = themes_dir .. "/default/icons/powerarrow/mail.png"
---{{----------------------------------------------------------------------------
-
-
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
--- taglist_[bg|fg]_[focus|urgent|occupied|empty]
--- tasklist_[bg|fg]_[focus|urgent]
--- titlebar_[bg|fg]_[normal|focus]
--- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- mouse_finder_[color|timeout|animate_timeout|radius|factor]
--- Example:
---theme.taglist_bg_focus = "#ff0000"
-
--- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
-
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
